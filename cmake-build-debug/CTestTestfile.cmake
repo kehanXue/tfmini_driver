594d79ad89c9908f5e46cfbce7a30568914bb2dc
Kehan.Xue <kehan.xue@gmail.com>
2019-08-06 23:52:24 +0800

    update .gitignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kehan/focus_ws/src/tfmini_driver
-# Build directory: /home/kehan/focus_ws/src/tfmini_driver/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
